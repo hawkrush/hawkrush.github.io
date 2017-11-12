e7b8c171f8032297fe6bb71dc78d147415b1df4e
Nicholas Sarvi <nicholas.sarvi@mail.utoronto.ca>
2017-11-12 00:26:47 -0500

    Update #1: Did video background and prototype title

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/* Importing libraries */
+/* import $ from 'jquery'; */
+$(document).ready(funtion()) {
+	var tag = document.createElement('script');
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0]
+}
